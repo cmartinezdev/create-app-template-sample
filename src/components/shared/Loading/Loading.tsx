e8900b84f11ae3c030eae5aaead0659304cd4ffb
Carlos Martínez González <carlos.martinez@biko2.com>
2018-02-19 13:23:42 +0100

    Improved loading component

--- src/components/shared/Loading/Loading.tsx
+++ src/components/shared/Loading/Loading.tsx
@@ -3,7 +3,20 @@ import { translate, TranslationFunction, Trans } from 'react-i18next';
 
 interface Props {
   t: TranslationFunction;
+  error: Boolean;
+  pastDelay: Boolean;
+  timedOut: Boolean;
 }
-const Loading: React.SFC<Props> = ({ t }) => <div>{t('loading')}...</div>;
+const Loading: React.SFC<Props> = ({ t, error, pastDelay, timedOut }) => {
+  if (error) {
+    return <div>Error!</div>;
+  } else if (timedOut) {
+    return <div>{t('toMuchTime')}...</div>;
+  } else if (pastDelay) {
+    return <div>{t('loading')}...</div>;
+  } else {
+    return null;
+  }
+};
 
 export default translate()(Loading);
