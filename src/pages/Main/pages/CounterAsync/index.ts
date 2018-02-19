513a0c4479b32281e433ca68eb7bba01f2d6c90c
Carlos Martínez González <carlos.martinez@biko2.com>
2018-02-19 16:27:18 +0100

    Make CounterSync and CounterAsync lazy loaded

--- src/pages/Main/pages/CounterAsync/index.ts
+++ src/pages/Main/pages/CounterAsync/index.ts
@@ -1,1 +1,13 @@
-export { default } from './CounterAsync';
+import * as Loadable from 'react-loadable';
+
+import Loading from 'src/components/shared/Loading';
+
+// export { default } from './CounterAsync';
+
+export default Loadable({
+  loader: () =>
+    import(/* webpackChunkName: "counter_async" */ './CounterAsync'),
+  loading: Loading, // Puede dar error si no está correctamente tipado
+  delay: 200,
+  timeout: 3000,
+});
--- src/pages/Main/pages/CounterSync/index.ts
+++ src/pages/Main/pages/CounterSync/index.ts
@@ -1,1 +1,12 @@
-export { default } from './CounterSync';
+import * as Loadable from 'react-loadable';
+
+import Loading from 'src/components/shared/Loading';
+
+// export { default } from './CounterSync';
+
+export default Loadable({
+  loader: () => import(/* webpackChunkName: "counter_sync" */ './CounterSync'),
+  loading: Loading, // Puede dar error si no está correctamente tipado
+  delay: 200,
+  timeout: 3000,
+});
