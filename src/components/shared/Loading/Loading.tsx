3d00648e05eb169653bb0673cdba3d9209ecbe3a
Carlos Martínez González <carlos.martinez@biko2.com>
2018-02-19 13:42:41 +0100

    Removed unused import in Loading component

--- src/components/shared/Loading/Loading.tsx
+++ src/components/shared/Loading/Loading.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { translate, TranslationFunction, Trans } from 'react-i18next';
+import { translate, TranslationFunction } from 'react-i18next';
 
 interface Props {
   t: TranslationFunction;
