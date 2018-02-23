aa1adcf1439bf35a9e39621207a458c2073d5d11
Carlos Martínez González <carlos.martinez@biko2.com>
2018-02-23 11:43:02 +0100

    Redirect with saga after login

--- src/redux/modules/auth/auth.sagas.ts
+++ src/redux/modules/auth/auth.sagas.ts
@@ -1,13 +1,14 @@
 import { SagaIterator } from 'redux-saga';
-import { call, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest, takeEvery } from 'redux-saga/effects';
 import { authenticateActionCreator } from './auth.actions.creators';
 import { AuthenticatePayloadDone } from './auth.actions.interfaces';
 import { isType, AnyAction } from 'typescript-fsa';
 import { bindAsyncAction } from 'typescript-fsa-redux-saga-extended';
+import { push } from 'react-router-redux';
 
-import { sendPost } from 'src/services/httpClient';
+import { sendPost } from 'src/infrastructure/httpClient';
 
-export const authenticateHandler = function*(action: AnyAction): SagaIterator {
+const authenticateSaga = function*(action: AnyAction): SagaIterator {
   if (isType(action, authenticateActionCreator.started)) {
     const res = yield call(sendPost, 'https://reqres.in/api/login', {
       email: action.payload.user,
@@ -26,13 +27,31 @@ export const authenticateHandler = function*(action: AnyAction): SagaIterator {
   }
 };
 
+const redirectSaga = function*(action: AnyAction): SagaIterator {
+  if (isType(action, authenticateActionCreator.done)) {
+    yield put(push('/'));
+
+    return;
+  }
+};
+
+const authenticateHandler = bindAsyncAction(authenticateActionCreator)(
+  authenticateSaga,
+);
+
+const redirectHandler = redirectSaga;
+
 export const sagas = {
   authenticate: function* watchAuthenticate() {
     try {
-      yield takeLatest(
-        authenticateActionCreator.started,
-        bindAsyncAction(authenticateActionCreator)(authenticateHandler),
-      );
+      yield takeLatest(authenticateActionCreator.started, authenticateHandler);
+    } catch (error) {
+      //
+    }
+  },
+  redirect: function* watchAuthenticateRedirect() {
+    try {
+      yield takeEvery(authenticateActionCreator.done, redirectHandler);
     } catch (error) {
       //
     }
